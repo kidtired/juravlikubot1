248e42bd7009eb02db5c5a7909c536aef21ca134
kindtired <kindtired@gmail.com>
2021-08-17 21:26:12 +0400

    pressF

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 admin_id = os.getenv("admin_id")
+BOT_URL = os.getenv("BOT_URL")
 
 URL_APPLES = os.getenv("URL_APPLES")
 URL_PEAR = os.getenv("URL_PEAR")
